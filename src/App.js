8b1f8b3affffa5e0bacd876ae793927e7a1753be
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-30 02:25:01 -0600

    added Security component from okta in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
+
+const config = {
+  clientId: '0oaapt0s0EjOyHmVd4x6',
+  issuer: 'https://dev-861398.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:3000/implicit/callback',
+  scopes: ['openid', 'profile', 'email'],
+  pkce: true
+};
 
 function App() {
   const CALLBACK_PATH = '/implicit/callback'
   return (
     <div>
-      <Route path={CALLBACK_PATH} />
-
+      <Security {...config}>
+        <Route path={CALLBACK_PATH} />
+      </Security>
     </div>
   );
 }
