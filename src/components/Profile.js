246867644db56964380ecaab642889710b45659f
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-30 03:07:53 -0600

    access user info in Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useOktaAuth } from '@okta/okta-react';
 
-const Profile = () => {
+const Profile = () => { 
+    const { authState, authService } = useOktaAuth();
+    const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        if (!authState.isAuthenticated) {
+            setUserInfo(null);
+        } else {
+            console.log(authState)
+            authService.getUser().then((info) => {
+                console.log(info)
+                setUserInfo(info);
+        });
+        }
+    }, [authState, authService]);
+  
     return (
         <div>
-            <p>yay it worked</p>
+        { userInfo && 
+            <div>
+            <p>Welcome back, {userInfo.name}!</p>
+            <p></p>
+            </div>
+        }
         </div>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
