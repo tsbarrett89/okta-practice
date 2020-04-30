f125735635c66bf5752d2d5c3c2418a6578d4746
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-30 02:13:50 -0600

    Route and CALLBACK_PATH added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 function App() {
+  const CALLBACK_PATH = '/implicit/callback'
   return (
     <div>
+      <Route path={CALLBACK_PATH} />
 
     </div>
   );
